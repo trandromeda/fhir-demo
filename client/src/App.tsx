9b5063fee3e54410238aeeb9328007164f1fe15b
Andy Tran <transient.tran@gmail.com>
2021-05-12 15:30:55 -0400

    set up functional app; pass cleaned data to client; organize components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
+import axios from "axios";
 import Patients from "./components/patients/Patients";
 
 interface IPatient {
-  fullUrl: any;
-  resource: any;
+  id: string;
+  name?: Array<any>;
+  birthDate?: string;
+  gender?: string;
+  age?: number;
 }
-function App() {
-  const [showPatients, setShowPatients] = useState(false);
 
-  const handleShowPatients = () => {
-    setShowPatients(true);
+function App() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [patients, setPatients] = useState<IPatient[]>([]);
+
+  const handleGetPatients = () => {
+    setIsLoading(true);
+    axios
+      .get("/patients?birthdate=1951-01-01")
+      .then((res: { data: { patients: IPatient[]; next: string } }) => {
+        const body = res.data;
+        console.log(body);
+        setPatients(res.data.patients);
+        setIsLoading(false);
+      });
   };
 
   return (
     <div>
-      <button onClick={handleShowPatients}>Fetch Patients</button>
-      {showPatients && <Patients />}
+      <button onClick={handleGetPatients}>Fetch Patients</button>
+
+      {isLoading && <p>Loading...</p>}
+
+      {!isLoading && <Patients patients={patients} />}
     </div>
   );
 }
--- server/routes/patients.js
+++ server/routes/patients.js
@@ -1,40 +1,77 @@
 var express = require("express");
 var axios = require("axios");
 var router = express.Router();
+var _ = require("lodash");
 
-// const instance = axios.create({
-//   baseURL: "http://hapi.fhir.org/baseR4/",
-//   timeout: 1000,
-// });
+const { DateTime, Interval } = require("luxon");
 
-const getPatients = async (url) => {
-  const response = await axios.get(url);
-  const responseData = response.data;
-
-  if (
-    responseData.link &&
-    responseData.link[1] &&
-    responseData.link[1].relation === "next"
-  ) {
-    const nextUrl = responseData.link[1].url;
-    return responseData;
-    // return data.concat(await getPatients(nextUrl));
-  } else {
-    return responseData;
-  }
-};
-
-router.get("/", function (req, res, next) {
-  res.send("patients data");
+const instance = axios.create({
+  baseURL: "http://hapi.fhir.org/baseR4/",
+  timeout: 1000,
 });
 
-router.get("/test", async (req, res, next) => {
-  const data = await getPatients(
-    "http://hapi.fhir.org/baseR4/Patient?birthdate=ge1951-01-01&name=andy"
-  );
-  //   console.log("final result");
-  //   console.log(data);
-  res.send(data);
+const getPatients = (query) => {
+  const response = instance.get(`Patient?${query}`);
+  return response;
+
+  /** to recursively fetch all of the bundle */
+  const data = response.data;
+  const results = [data];
+  // if (data.link[1] && data.link[1].relation === "next") {
+  //   const nextUrl = data.link[1].url;
+  //   return results.concat(await getPatients(nextUrl));
+  // } else {
+  // }
+  // return results;
+};
+
+const calculateAge = (dateString) => {
+  const birthDate = DateTime.fromISO(dateString);
+  const now = DateTime.now();
+
+  const yearsBetween = Interval.fromDateTimes(birthDate, now);
+  const yearsObject = yearsBetween.toDuration("years").toObject();
+
+  return Math.floor(yearsObject.years);
+};
+
+const getGivenName = (name) => {
+  return name.given ? name.given.join(" ") : "n/a";
+};
+
+const getName = (patientName) => {
+  const officialName = patientName
+    ? _.find(patientName, { use: "official" }) || patientName[0]
+    : { text: "n/a" };
+
+  return officialName.text
+    ? officialName.text
+    : `${officialName.family}, ${getGivenName(officialName)}`;
+};
+
+const transformPatientsOut = (patients) => {
+  return patients.map((patient) => {
+    return {
+      id: patient.resource.id,
+      name: getName(patient.resource.name),
+      birthDate: patient.resource.birthDate,
+      gender: patient.resource.gender,
+      age: calculateAge(patient.resource.birthDate),
+    };
+  });
+};
+
+router.get("/", async (req, res, next) => {
+  const birthdate = req.params.birthdate;
+  const query = birthdate ? `birthdate=ge${birthdate}` : "";
+  const response = await getPatients(query);
+
+  const patientData = transformPatientsOut(response.data.entry);
+
+  res.json({
+    patients: patientData,
+    next: _.find(response.data.link, { relation: "next" }),
+  });
 });
 
 module.exports = router;
